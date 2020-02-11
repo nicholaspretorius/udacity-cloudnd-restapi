d00d87066b8a675b925626647f3d791dfd32b142
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-02-11 11:11:19 +0200

    chore: Update endpoint

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.prod.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ncp-udacity-c2-restapi-dev.us-east-1.elasticbeanstalk.com/api/v0/'
 };
 
 /*
